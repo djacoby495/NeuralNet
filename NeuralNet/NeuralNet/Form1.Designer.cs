4dfd36cac9d6ec1bf86306539c718bf6314ff38a
djacoby495 <78579364+djacoby495@users.noreply.github.com>
2021-07-17 15:26:58 -0700

    Add files via upload

---
+++ NeuralNet/NeuralNet/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace NeuralNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trainNN = new System.Windows.Forms.Button();
+            this.testNN = new System.Windows.Forms.Button();
+            this.createNN = new System.Windows.Forms.Button();
+            this.ResultInfo = new System.Windows.Forms.ListBox();
+            this.EpochTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LayerTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // trainNN
+            // 
+            this.trainNN.Location = new System.Drawing.Point(50, 276);
+            this.trainNN.Name = "trainNN";
+            this.trainNN.Size = new System.Drawing.Size(202, 76);
+            this.trainNN.TabIndex = 1;
+            this.trainNN.Text = "TrainNN";
+            this.trainNN.UseVisualStyleBackColor = true;
+            this.trainNN.Click += new System.EventHandler(this.trainNN_Click);
+            // 
+            // testNN
+            // 
+            this.testNN.Location = new System.Drawing.Point(50, 358);
+            this.testNN.Name = "testNN";
+            this.testNN.Size = new System.Drawing.Size(202, 76);
+            this.testNN.TabIndex = 2;
+            this.testNN.Text = "TestNN";
+            this.testNN.UseVisualStyleBackColor = true;
+            this.testNN.Click += new System.EventHandler(this.testNN_Click);
+            // 
+            // createNN
+            // 
+            this.createNN.Location = new System.Drawing.Point(50, 194);
+            this.createNN.Name = "createNN";
+            this.createNN.Size = new System.Drawing.Size(202, 76);
+            this.createNN.TabIndex = 3;
+            this.createNN.Text = "CreateNN";
+            this.createNN.UseVisualStyleBackColor = true;
+            this.createNN.Click += new System.EventHandler(this.createNN_Click);
+            // 
+            // ResultInfo
+            // 
+            this.ResultInfo.FormattingEnabled = true;
+            this.ResultInfo.ItemHeight = 16;
+            this.ResultInfo.Location = new System.Drawing.Point(293, 43);
+            this.ResultInfo.Name = "ResultInfo";
+            this.ResultInfo.Size = new System.Drawing.Size(365, 548);
+            this.ResultInfo.TabIndex = 5;
+            // 
+            // EpochTextBox
+            // 
+            this.EpochTextBox.Location = new System.Drawing.Point(50, 63);
+            this.EpochTextBox.Name = "EpochTextBox";
+            this.EpochTextBox.Size = new System.Drawing.Size(237, 22);
+            this.EpochTextBox.TabIndex = 6;
+            this.EpochTextBox.Text = "20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total Epochs to train for: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Layer Layout (comma seperated): ";
+            // 
+            // LayerTextBox
+            // 
+            this.LayerTextBox.Location = new System.Drawing.Point(50, 108);
+            this.LayerTextBox.Name = "LayerTextBox";
+            this.LayerTextBox.Size = new System.Drawing.Size(237, 22);
+            this.LayerTextBox.TabIndex = 9;
+            this.LayerTextBox.Text = "10, 10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(240, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ex. with 3 layers of 20 neurons each:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "20, 20, 20";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 440);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 151);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 627);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LayerTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EpochTextBox);
+            this.Controls.Add(this.ResultInfo);
+            this.Controls.Add(this.createNN);
+            this.Controls.Add(this.testNN);
+            this.Controls.Add(this.trainNN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button trainNN;
+        private System.Windows.Forms.Button testNN;
+        private System.Windows.Forms.Button createNN;
+        private System.Windows.Forms.ListBox ResultInfo;
+        private System.Windows.Forms.TextBox EpochTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LayerTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NeuralNet/NeuralNet/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNet
+{
+    public partial class Form1 : Form
+    {
+        private List<MNISTImage> training_data = new List<MNISTImage>();
+        private List<MNISTImage> test_data = new List<MNISTImage>();
+        private static Random r = new Random();
+        private static NeuralNetwork NN;
+        private int epochs = 100;
+
+        class NeuralNetwork
+        {
+            public int[] layers;
+            public double[][] neurons;
+            public double[][] biases;
+            public double[][][] weights;
+
+            public double fitness = 0;
+
+            public double learningRate = 0.01;
+            public double cost = 0;
+
+            public NeuralNetwork(int[] layers)
+            {
+                //initialize layers array
+                //create array for layers pointer and populate with passed in array values
+                this.layers = new int[layers.Length];
+                for (int i = 0; i < layers.Length; i++)
+                    this.layers[i] = layers[i];
+
+                //initialize neurons - possibly change as it's kinda jank
+                List<double[]> neuronsList = new List<double[]>();
+                for (int i = 0; i < layers.Length; i++)
+                    neuronsList.Add(new double[layers[i]]);
+                neurons = neuronsList.ToArray();
+
+                //initialize biases
+                List<double[]> biasList = new List<double[]>();
+                for (int i = 0; i < layers.Length; i++)
+                {
+                    double[] bias = new double[layers[i]];
+                    for (int j = 0; j < layers[i]; j++)
+                        bias[j] = r.NextDouble();
+                    biasList.Add(bias);
+                }
+                biases = biasList.ToArray();
+
+                //initialize weights
+                List<double[][]> weightsList = new List<double[][]>();
+                for (int i = 1; i < layers.Length; i++)
+                {
+                    List<double[]> layerWeightsList = new List<double[]>();
+                    int previousLayerNeurons = layers[i - 1];
+                    for (int j = 0; j < neurons[i].Length; j++)
+                    {
+                        double[] neuronWeights = new double[previousLayerNeurons];
+                        for (int k = 0; k < previousLayerNeurons; k++)
+                            neuronWeights[k] = r.NextDouble();
+                        layerWeightsList.Add(neuronWeights);
+                    }
+                    weightsList.Add(layerWeightsList.ToArray());
+                }
+                weights = weightsList.ToArray();
+            }
+
+            public double[] FeedForward(double[] inputs)
+            {
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    neurons[0][i] = inputs[i];
+                }
+                for (int i = 1; i < layers.Length; i++)
+                {
+                    int layer = i - 1;
+                    for (int j = 0; j < neurons[i].Length; j++)
+                    {
+                        double value = 0.0;
+                        for (int k = 0; k < neurons[i - 1].Length; k++)
+                        {
+                            double value1 = weights[i - 1][j][k];
+                            double value2 = neurons[i - 1][k];
+                            value += value1 * value2;
+                        }
+                        neurons[i][j] = sigmoid_output(value + biases[i][j]);
+                    }
+                }
+                return neurons[neurons.Length - 1];
+            }
+
+            public void BackPropagate(double[] inputs, int[] expected)
+            {
+                double[] output = FeedForward(inputs);
+
+                //find cost of NN (not used for anything but analysis
+                cost = 0;
+                for (int i = 0; i < output.Length; i++)
+                    cost += Math.Pow(output[i] - expected[i], 2);
+                cost = cost / 2;
+
+                double[][] error;
+
+                //initialize gamma
+                List<double[]> errorList = new List<double[]>();
+                for (int i = 0; i < layers.Length; i++)
+                    errorList.Add(new double[layers[i]]);
+                error = errorList.ToArray();
+
+                //calculate error for the output layer of neurons
+                int layer = layers.Length - 2;
+                for (int i = 0; i < output.Length; i++)
+                    error[layers.Length - 1][i] = (output[i] - expected[i]) * sigmoid_derivative(output[i]);
+
+
+                for (int i = 0; i < layers[layers.Length-1]; i++)
+                {
+                    biases[layers.Length - 2][i] -= error[layers.Length - 1][i] * learningRate;
+                    for (int j = 0; j < layers[layers.Length - 2]; j++)
+                        weights[layers.Length - 2][i][j] -= error[layers.Length - 1][i] * neurons[layers.Length - 2][j] * learningRate;
+                }
+
+                //hidden layers
+                for (int i = layers.Length - 2; i > 0; i--)
+                {
+                    layer = i - 1;
+                    for (int j = 0; j < layers[i]; j++)
+                    {
+                        error[i][j] = 0;
+                        for (int k = 0; k < error[i + 1].Length; k++)
+                            error[i][j] += error[i + 1][k] * weights[i][k][j];
+
+                        error[i][j] *= sigmoid_derivative(neurons[i][j]);
+                    }
+
+                    for (int j = 0; j < layers[i]; j++)
+                    {
+                        biases[i - 1][j] -= error[i][j] * learningRate;
+                        for (int k = 0; k < layers[i - 1]; k++)
+                            weights[i - 1][j][k] -= error[i][j] * neurons[i - 1][k] * learningRate;  //change weights
+                    }
+                }
+            }
+
+            
+        }
+
+        class MNISTImage
+        {
+            //holds the ACTUAL value of the image
+            public int result;
+            //holds the pixels of the image, range from 0 - 1
+            public List<double> pixel = new List<double>();
+        }
+
+        //calculates sigmoid value an returns as a double
+        private static double sigmoid_output(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        private static double sigmoid_derivative(double x)
+        {
+            double y = sigmoid_output(x);
+            return y * (1.0 - y);
+        }
+
+        private void loadData()
+        {
+            string line;
+            int counter = 0;
+
+            Console.WriteLine("STARTED LOADING TRAINING-DATA");
+            TextReader reader = File.OpenText("mnist_train.csv");
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(',');
+                MNISTImage tempImage = new MNISTImage();
+                tempImage.result = int.Parse(tokens[0]);
+
+                for (int i = 1; i < tokens.Count(); i++)
+                {
+                    tempImage.pixel.Add((int.Parse(tokens[i])) / 255.0);
+                }
+                training_data.Add(tempImage);
+
+                counter++;
+                if (counter % 5000 == 0)
+                    Console.WriteLine(counter + " items loaded");
+            }
+            Console.WriteLine("DONE LOADING TRAINING-DATA");
+
+            Console.WriteLine("STARTED LOADING TESTING-DATA");
+            reader = File.OpenText("mnist_test.csv");
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(',');
+                MNISTImage tempImage = new MNISTImage();
+                tempImage.result = int.Parse(tokens[0]);
+
+                for (int i = 1; i < tokens.Count(); i++)
+                {
+                    tempImage.pixel.Add((int.Parse(tokens[i])) / 255.0);
+                }
+                test_data.Add(tempImage);
+
+                counter++;
+                if (counter % 5000 == 0)
+                    Console.WriteLine(counter + " items loaded");
+            }
+        }
+
+        private void testNet()
+        {
+            int correct_results = 0;
+            int total_tests = 0;
+
+            Console.WriteLine("TESTING");
+            double[] result_neurons;
+            for (int i = 0; i < test_data.Count(); i++)
+            {
+                result_neurons = NN.FeedForward(test_data[i].pixel.ToArray());
+
+                double max_value = 0.0;
+                int max_index = 99;
+                for (int x = 0; x < result_neurons.Length; x++)
+                {
+                    if (result_neurons[x] > max_value)
+                    {
+                        max_value = result_neurons[x];
+                        max_index = x;
+                    }
+                }
+
+                if (max_index == test_data[i].result)
+                {
+                    correct_results++;
+                }
+                total_tests++;
+            }
+            string printline = correct_results + " / " + total_tests;
+            ResultInfo.Items.Add(printline);
+            Console.WriteLine(printline);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createNN_Click(object sender, EventArgs e)
+        {
+            //create NN object, should be persistant through all button pushes
+            //parse total epochs from textbox
+            epochs = Int32.Parse(EpochTextBox.Text);
+
+            //parse layers into array to pass to NN on creation
+            string[] words = LayerTextBox.Text.Split(',');
+            int[] layersArr = new int[words.Length + 2];
+            layersArr[0] = 784;  //trivial values (input values)
+            layersArr[words.Length + 1] = 10; //trival values (output nodes)
+            int i = 1;
+            foreach (var word in words)
+            {
+                layersArr[i] = Int32.Parse(word);
+                i++;
+            }
+            
+            //creating NN
+            NN = new NeuralNetwork(layersArr);
+            Console.WriteLine("done creating and randomizing NN");
+        }
+
+        private void trainNN_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+
+            loadData();
+
+            int[] expected_result = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < epochs; i++)
+            {
+                Console.WriteLine("training: " + (i + 1) + " / " + epochs);
+
+                counter = 0;
+                for (int j = 0; j < training_data.Count(); j++)
+                {
+                    if (counter % 10000 == 0)
+                        Console.WriteLine(counter + "processed (train)");
+                    expected_result[training_data[j].result] = 1;
+                    NN.BackPropagate(training_data[j].pixel.ToArray(), expected_result);
+                    expected_result[training_data[j].result] = 0;
+                    counter++;
+                }
+
+                testNet();
+            }
+        }
+
+        private void testNN_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            int correct_results = 0;
+            int total_tests = 0;
+
+            counter = 0;
+            double[] result_neurons;
+            for (int i = 0; i < test_data.Count(); i++)
+            {
+                if (counter % 1000 == 0)
+                    Console.WriteLine(counter + "processed (test)");
+                result_neurons = NN.FeedForward(test_data[i].pixel.ToArray());
+
+                double max_value = 0.0;
+                int max_index = 99;
+                for (int x = 0; x < result_neurons.Length; x++)
+                {
+                    if (result_neurons[x] > max_value)
+                    {
+                        max_value = result_neurons[x];
+                        max_index = x;
+                    }
+                }
+
+                if (max_index == test_data[i].result)
+                {
+                    correct_results++;
+                }
+
+                total_tests++;
+                counter++;
+            }
+            string printline = correct_results + " / " + total_tests;
+            ResultInfo.Items.Add(printline);
+        }
+    }
+}
